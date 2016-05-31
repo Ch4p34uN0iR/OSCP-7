e53d6b66563ed1197a7e27494215ae7f6d13812e
ma11ock <thema11ock@gmail.com>
2016-05-31 11:06:01 -0500

    adding masscan nmap script

---
+++ imasscan.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/ruby
+require 'optparse'
+
+options = {:ports => nil, :ip => nil}
+summary = ""
+ARGV.push('-h') if ARGV.empty?
+parser = OptionParser.new do|opts|
+	opts.banner = "Usage: imasscan.rb [options]"
+	opts.on('-i', '--ip ip', 'ip address') do |ip|
+		options[:ip] = ip;
+    puts ip
+	end
+
+  opts.on("-p","--ports x,y", "-[a-z]", "Array", "Enter port or ports to scan comma seperated or range") do |port|
+    puts port
+    options[:ports] = port
+  end
+
+  opts.on('-h', '--help', 'Displays Help') do
+		puts opts
+		exit
+	end
+  summary = opts.summarize
+end.parse!
+
+def run_masscan
+  system "bash", "-c", "masscan -p#{:ports} #{:ip}"
+end
+
+def run_nmap
+
+end
+
+run_masscan
