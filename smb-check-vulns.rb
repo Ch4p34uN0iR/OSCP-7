78e134f5dde3a30ff9e6b2cd8c6f1a5ed8208a94
ma11ock <thema11ock@gmail.com>
2016-06-14 15:26:50 -0500

    indent off ew

--- smb-check-vulns.rb
+++ smb-check-vulns.rb
@@ -9,18 +9,18 @@ summary = ""
 
 ARGV.push("-h") if ARGV.empty?
 parse = OptionParser.new do |opts|
-	opts.banner = "Usage: smb-check-vulns.rb [options]"
-	opts.on("-p","--ports x,y","-[a-z]","Enter port or ports to scan comma seperated") do |port|
-		options[:ports] = port
-	end
-	opts.on("-i","--ip x,y","ip address") do |ip|
-		options[:ip] = ip
-	end 
-	opts.on("-h", "--help","Displays help")do
-		puts opts
-		exit
-	end
-summary = opts.summarize
+  opts.banner = "Usage: smb-check-vulns.rb [options]"
+  opts.on("-p","--ports x,y","-[a-z]","Enter port or ports to scan comma seperated") do |port|
+    options[:ports] = port
+  end
+  opts.on("-i","--ip x,y","ip address") do |ip|
+    options[:ip] = ip
+  end 
+  opts.on("-h", "--help","Displays help")do
+    puts opts
+    exit
+  end
+  summary = opts.summarize
 end.parse!
 
 @scripts=["smb-vuln-conficker.nse","smb-vuln-cve2009-3103.nse","smb-vuln-ms06-025.nse","smb-vuln-ms07-029.nse","smb-vuln-ms08-067.nse","smb-vuln-ms10-054.nse","smb-vuln-ms10-061.nse","smb-vuln-regsvc-dos.nse"]
@@ -28,11 +28,11 @@ end.parse!
 @ip    = options[:ip]
 @output= []
 def check_vulns
-	@scripts.each{|vuln|puts "[+] checking #{vuln}"; @output << %x[nmap -v #{@ip} -p #{@ports} --script=#{vuln}]}
+  @scripts.each{|vuln|puts "[+] checking #{vuln}"; @output << %x[nmap -v #{@ip} -p #{@ports} --script=#{vuln}]}
 end
 
 def format_output
-	@output.each{|result| puts "\n" + result.partition("VULNERABLE:").first.partition("report for ").last + result.partition("VULNERABLE:").last.partition("NSE: Script Post").first if result.include?("VULNERABLE")}
+  @output.each{|result| puts "\n" + result.partition("VULNERABLE:").first.partition("report for ").last + result.partition("VULNERABLE:").last.partition("NSE: Script Post").first if result.include?("VULNERABLE")}
 end
 
 check_vulns
